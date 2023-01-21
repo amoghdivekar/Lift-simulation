dabb6fa6d429030d3b7291c1f62b4f680ca8415a
Amogh Divekar <amoghdivekar22@gmail.com>
2023-01-21 14:57:13 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const generateBtn = document.querySelector("#generate-btn");
+const floorInput = document.querySelector("#floor-input");
+const liftInput = document.querySelector("#lift-input");
+
+generateBtn.addEventListener("click", clickHandler);
+
+function clickHandler() {
+    // console.log("clicked");
+}
